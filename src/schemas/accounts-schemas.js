dc709ab906bb849b72506e3856d701f64ca414cd
Wesley da Silva Ribeiro <wesleysilvaribeiro9@gmail.com>
2023-12-11 10:21:12 -0300

    refactor: changed field account field name to userName

--- src/schemas/accounts-schemas.js
+++ src/schemas/accounts-schemas.js
@@ -3,7 +3,7 @@ import joi from 'joi';
 export const signUpSchema = joi.object({
     email: joi.string().trim().email().required(),
     password: joi.string().trim().min(3).required(),
-    name: joi.string().trim().required(),
+    userName: joi.string().trim().required(),
 });
 
 export const signInSchema = joi.object({
